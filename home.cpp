9a01d49ed36007d9010340a717f0d954460b4298
Lee Jae Yeon <wodus7792@naver.com>
2022-11-12 16:41:14 +0900

    commit_test

--- home.cpp
+++ home.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    int n;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    //n보다 작은 정수 사용
     int num;
+    int sum=0;
+    int n;
+    cin>>n;
     
-
-    for(int i=0; i<t; i++){
-        cin>>n;
-        int score=0;
-        for(int j=0; j<n; j++){
-            cin>>num;
-            score+=num;
-        }
-        cout<<score<<endl;
+    for(int i=0; i<n; i++){
+        sum+=i;            //n아래의 숫자 다 더하기(n-1개)
     }
-    
-    
 
+    for(int i=0; i<n; i++){
+        cin>>num;
+        sum-=num;           //중복되지 않은 것들 빼고나면 중복된 숫자만 음수값으로 나옴.
+    }
+
+    sum*=-1;
+    cout<<sum;      
 
     return 0;
-    
 }
